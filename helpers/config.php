9566ed8268e8f49a4c8fae901bde35bf04224337
unknown <24yn0106@jec.ac.jp>
2025-07-17 19:59:58 +0900

    DB接続文字列を732実習室からAzureSQLデータベースへ変更

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server = tcp:24yn0106.database.windows.net,1433');
+    define('DSN', 'sqlsrv:server = tcp:24yn0106.database.windows.net,1433; Database = 24yn0106');
     define('DB_USER', 'yndb');
     define('DB_PASSWORD', 'Pa$$word1234');
